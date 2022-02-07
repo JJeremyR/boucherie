7b4e1e8cc18682bbf668328e12580c77acb52708
JJeremyR <jjuliuszeinger@gmail.com>
2022-02-07 21:37:57 +0100

    vue catalogue

---
+++ src/Classes/Search.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Classes;
+
+use App\Entity\Categories;
+
+class Search
+{
+    /**
+     * @var string 
+     */
+    public $string = '';
+
+    /**
+     * @var Categories[]
+     */
+    public $categories = [];
+}
