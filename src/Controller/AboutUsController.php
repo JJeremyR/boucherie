39736cd915644d256d1a80589a4a3b092532786e
JJeremyR <jjuliuszeinger@gmail.com>
2022-03-02 09:21:22 +0100

    Merge branch 'dev-jerem' into TestBranch

--- src/Controller/AboutUsController.php
+++ src/Controller/AboutUsController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Classes\Cart;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -11,8 +12,10 @@ class AboutUsController extends AbstractController
     /**
      * @Route("/qui-sommes-nous", name="about_us")
      */
-    public function index(): Response
+    public function index(Cart $cart): Response
     {
-        return $this->render('about_us/index.html.twig');
+        return $this->render('about_us/index.html.twig', [
+            'cart' => $cart->getFull(),
+        ]);
     }
 }
